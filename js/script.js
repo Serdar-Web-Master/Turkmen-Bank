d60d7aa08f8a80298bd64229226c1e8e5a730ac1
Serdar <serdarcholukov@yandex.com>
2022-12-16 00:27:25 +0500

    modal header

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ const modal__window = document.querySelector('.modal__window');
 setTimeout(() => {
    modal__window.style.display = "none"
 }, 4000);
+
+const modalMain = document.querySelector('.modal__main');
+const wrapper = document.querySelector('.wrapper');
+
+const enterBlock = document.querySelector('.enter-block');
+enterBlock.addEventListener("click",()=>{
+   modalMain.style.display = "block";
+   wrapper.style.display = "none"
+})
