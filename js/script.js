2a12c7d8dc7d515291d65a32b7d587e49830f625
Serdar <serdarcholukov@yandex.com>
2022-12-15 16:19:56 +0500

    modal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 import { a, b} from "./vars.js";
-
 console.log(a,b);
+
+const modal__window = document.querySelector('.modal__window');
+setTimeout(() => {
+   modal__window.style.display = "none"
+}, 4000);
